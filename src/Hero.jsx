a87266a628adc6279b31311d0b1baf7bb44f550f
Yogesh Vanzara <yogeshvanzara98@gmail.com>
2023-05-30 22:17:19 +0530

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,23 @@
+import heroImg from "./assets/hero.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi quia
+            quibusdam esse sit illo inventore perspiciatis obcaecati nam
+            voluptates aliquid. Accusamus, dignissimos sunt.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
